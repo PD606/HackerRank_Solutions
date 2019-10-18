ec8a109d55d37050d03756b77f445e1383f5dd6f
PD606 <praveenh.cs17@sahyadri.edu.in>
2019-10-18 18:20:30 +0530

    Updating Insertnode

---
+++ Insertnode_in_LinkedList.py
@@ -0,0 +1,97 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data1, position):
+    #print(position
+    count=0
+    temp=head
+    temp.data=head.data
+    temp.next=head.next
+    while(count != position):
+        #print(head.data)
+        temp=temp.next
+        count=count+1
+    temp1=temp
+    p=temp
+    temp.data=data1
+    temp.next=temp1
+    temp1=p
+    #print(temp1.data)
+
+    return head
+        
+    
+    #head.data=data
+    #head.next=temp
+    #temp.next='\0'
+    #print(temp.data)    
+        #print(head.next.data)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
