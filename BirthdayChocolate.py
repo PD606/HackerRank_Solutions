bb0e7bb76da93e21de34f0ed90a389debd751488
PD606 <praveenh.cs17@sahyadri.edu.in>
2020-05-30 12:39:22 +0530

    added

--- BirthdayChocolate.py
+++ BirthdayChocolate.py
@@ -8,26 +8,35 @@ import sys
 
 # Complete the birthday function below.
 def birthday(s, d, m):
-    # print(s)
-    i=0
     c=0
+    i=0
+    s.append(0)
+    while(i<len(s)):
+        sum=0
+        sm=s[i]
+        j=i+1
+        # if(k==len(s)):
+        #     break
+        try:
+            while(j<m+i):
+                sum=sum+s[j]
+            # print(sum)
+                j+=1
 
-    if(len(s)==1):
-        if(d == 1):
-            return 1
-    
-    else:
-        while(i<len(s)):
-            sm=0
-            for j in range(i+1,m+1):
-                sm=sm+s[i]+s[j]
-            # print(sm)
-                if(sm == d):
-                    c+=1
-            i+=1
+        except:
+            pass
 
-    return c
+        i+=1
+        ts=sum+sm
 
+        # print(ts)
+
+        if(ts==d):
+            c+=1
+
+  
+
+    return (c)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
